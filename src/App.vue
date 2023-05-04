f21808e6a93a4baafc06243c235012c2f3b57ae3
HerlintonFilho <herlintoborba5@gmail.com>
2023-05-04 11:13:39 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,66 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div class="column is-half is-offset-one-quarter">
+      <img src="./assets/pokebola.png" width="300" height="300">
+      <hr>
+      <h4 class="is-size-4">Pokedéx</h4>
+      <input class="input is-rounded" type="text" placeholder="Buscar Pokemon" v-model="busca">
+      <button class="button is-normal is-fullwidth is-success" id="buscarBtn" @click="buscar">Buscar</button>
+      <div v-for="(poke, index) in filteredPokemons" :key="poke.url">
+      <Pokemon :name="poke.name" :url="poke.url" :num="index+1"/>
+    </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
+import axios from 'axios';
+import Pokemon from './components/Pokemon.vue'
+export default{
   name: 'App',
+  data(){
+    return{
+      pokemons: [],
+      filteredPokemons: [],
+      busca: ''
+    }
+  },
+  created: function(){
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then(res =>{
+      console.log("Sucess")
+      this.pokemons = res.data.results
+      this.filteredPokemons = res.data.results
+    })
+  },
   components: {
-    HelloWorld
+    Pokemon
+  },
+  methods:{
+    buscar: function(){
+      this.filteredPokemons = this.pokemons;
+      if(this.busca == '' || this.busca == ' '){
+        this.filteredPokemons = this.pokemons
+      }else{
+        this.filteredPokemons = this.pokemons.filter(pokemon => pokemon.name == this.busca)
+      }
+    }
+  },
+  computed:{
+    /*
+    resultado: function(){
+      if(this.busca == '' || this.busca == ' '){
+        return this.pokemons;
+      }else{
+        return this.pokemons.filter(pokemon => pokemon.name == this.busca)
+      }
+    }
+    */
   }
 }
 </script>
-
 <style>
 #app {
+  background-color: aquamarine;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -23,4 +68,10 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+#buscarBtn{
+  margin-top: 2%;
+  margin-bottom: 2%;
+}
+
 </style>
